d133a3a6e709d50ba6d951a354535df29239ca96
meyg6 <meyg6@DESKTOP-558Q9NR>
2018-10-20 20:32:55 +0300

    Add project files.

---
+++ Project01_9628_1304_dotNet5779/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project01_9628_1304_dotNet5779
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
